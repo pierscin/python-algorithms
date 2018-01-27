8b26901fb5d867c338ecce4eb8fff13992c17205
pierscin <michal@pierscin.ski>
2018-01-27 17:54:10 +0100

    [alg4]BST

--- algs4/data_structures.py
+++ algs4/data_structures.py
@@ -158,29 +158,49 @@ class MaxHeapOnList:
 class Bst:
     """Binary Search Tree."""
 
+    # TODO: size
+
     class Node:
         """Bst node."""
 
-        def __init__(self, key: Any, val, N):
+        def __init__(self, key: Any, val):
             self.key = key
             self.val = val
-            self.N = N
             self.left, self.right = None, None
 
     def __init__(self):
         self.root = None
 
-    def put(self, key, val: object):
+    def put(self, key, val: object) -> None:
+        """Upsert of value associated with the key."""
         if key is None: raise ValueError('None keys are not permitted')
 
         self.root = self._put(self.root, key, val)
 
     def get(self, key) -> object:
+        """Get value associated with key or None if such node does not exist."""
         if key is None: raise ValueError('None keys are not permitted')
 
         return self._get(self.root, key)
 
+    def min(self):
+        """Return min key."""
+        return self._min(self.root).key
+
+    def max(self):
+        """Return max key."""
+        return self._max(self.root).key
+
+    def remove_min(self) -> None:
+        """Remove node with min key."""
+        self.root = self._remove_min(self.root)
+
+    def remove_max(self) -> None:
+        """Remove node with max key."""
+        self.root = self._remove_max(self.root)
+
     def remove(self, key) -> None:
+        """Remove node with given key."""
         if key is None: raise ValueError('None keys are not permitted')
 
         self.root = self._remove(self.root, key)
@@ -189,11 +209,9 @@ class Bst:
 
         if node is None: return Bst.Node(key, val, 1)
 
-        if key > node.key:   self._put(node.right, key, val)
-        elif key < node.key: self._put(node.left, key, val)
+        if key > node.key:   node.right = self._put(node.right, key, val)
+        elif key < node.key: node.left = self._put(node.left, key, val)
         else: node.val = val
-
-        node.N = node.left.N + node.right.N + 1
 
         return node
 
@@ -205,5 +223,42 @@ class Bst:
 
         return node.val
 
+    def _min(self, node) -> 'Bst.Node':
+        return node if node.left is None else self._min(node.left)
+
+    def _max(self, node) -> 'Bst.Node':
+        return node if node.right is None else self._min(node.right)
+
+    def _remove_min(self, node) -> 'Bst.Node':
+        """Finds min node and returns it's RIGHT link."""
+        if node.left is None: return node.right
+
+        node.left = self._remove_min(node.left)
+
+        return node
+
+    def _remove_max(self, node) -> 'Bst.Node':
+        """Finds max node and returns it's LEFT link."""
+        if node.right is None: return node.left
+
+        node.right = self._remove_max(node.right)
+
+        return node
+
     def _remove(self, node: Optional['Bst.Node'], key) -> Optional['Bst.Node']:
-        raise NotImplementedError()
+        if node is None: return None
+
+        if key > node.key:   node.right = self._remove(node.right, key)
+        elif key < node.key: node.left = self._remove(node.left, key)
+        else:
+            if node.left is None: return node.right
+            if node.right is None: return node.left
+
+            # pierscin: Hibbard deletion
+            t = node
+
+            node = self._min(t.right)
+            node.right = self._remove_min(t.right)
+            node.left = t.left
+
+        return node
